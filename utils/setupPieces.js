ac63299c70787af6163d83d7a96555117d02884a
Benjamin Auder <benjamin.auder@somewhere>
2026-03-30 05:54:07 +0200

    Work on 8pieces. Updated setup_pieces (untested)

--- utils/setupPieces.js
+++ utils/setupPieces.js
@@ -7,20 +7,8 @@ export const FenUtil = {
   //                      "range": restrict piece position to some interval
   setupRow: function(arr, o) {
     let res = JSON.parse(JSON.stringify(arr));
-    if (o.randomness >= 1) {
+    if (o.randomness >= 1)
       res = Random.shuffle(arr);
-      if (o.range) {
-        Object.keys(range).forEach(function(p) {
-          res.indexOf(p) //TODO : finish here
-          //
-          //
-          //
-          //
-          //
-          //
-        });
-      }
-    }
     let flags = "";
     if (o.flags) {
       res.forEach((p, i) => {
@@ -29,6 +17,22 @@ export const FenUtil = {
       });
     }
     if (o.randomness >= 1) {
+      // NOTE: pieces involved in constraints are different...
+      if (o.range) {
+        Object.keys(o.range).forEach(function(p) {
+          // NOTE: assume only one piece of its kind
+          const pos = res.indexOf(p);
+          if (!o.range[p].includes(pos)) {
+            let new_pos;
+            do {
+              new_pos = o.range[p] [ Random.randInt(p.range.length) ]
+            }
+            // See https://stackoverflow.com/a/1098955
+            while (res[new_pos] in o.range);
+            [ res[new_pos], res[pos] ] = [ res[pos], res[new_pos] ];
+          }
+        });
+      }
       if (o.diffCol) {
         o.diffCol.forEach(p => {
           // Pieces of type p on different colors:
@@ -54,18 +58,19 @@ export const FenUtil = {
         o.between.forEach(b => {
           // p2 could be an array (EightPieces --> j, r)
           if (!Array.isArray(b["p2"]))
-            b["p2"] = [ b["p2"] ];
+            b["p2"] = [ b["p2"], b["p2"] ];
           // Locate p1. If appearing first, exchange with first p2.
           // If appearing last, exchange with last p2.
           const p1 = res.indexOf(b["p1"]);
-          const firstP2 = res.indexOf(b["p2"]),
-                lastP2 = res.lastIndexOf(b["p2"]);
-          if (p1 < firstP2 || p1 > lastP2) {
-            res[p1] = b["p2"];
-            if (p1 < firstP2)
-              res[firstP2] = b["p1"];
-            else //p1 > lastP2
-              res[lastP2] = b["p1"];
+          const firstP2 = res.indexOf(b["p2"][0]),
+                lastP2 = res.lastIndexOf(b["p2"][1]);
+          if (p1 < firstP2) {
+            res[p1] = b["p2"][0];
+            res[firstP2] = b["p1"];
+          }
+          else if (p1 > lastP2) {
+            res[p1] = b["p2"][1];
+            res[lastP2] = b["p1"];
           }
         });
       }
