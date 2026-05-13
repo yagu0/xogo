ab84e7d192fb684316dff689e9ce75a937143dd4
Benjamin Auder <benjamin.auder@somewhere>
2026-05-13 14:49:35 +0200

    update

--- initialize.sh
+++ initialize.sh
@@ -5,4 +5,4 @@ wget https://xogo.casa/extras.zip && unzip extras.zip
 cp js/parameters.js.dist js/parameters.js
 npm i
 cd pieces/Avalam && python generateSVG.py && cd ../..
-#cd pieces/Emergo && python generateSVG.py && cd ../..
+cd pieces/Emergo && python generateSVG.py && cd ../..
